fef244739e210d2d013f3f521c2cbdb23a5be84d
Daniel Mayo <daniel.mayo@neo4j.com>
2024-07-29 12:25:23 +0100

    Compress geonames data instead of checking it in to avoid LFS

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,22 @@
+import gzip
+from pathlib import Path
 import pytest
 from typing import Generator
 from importlib.resources import files, as_file
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def data_file_path() -> Generator[str, None, None]:
-    data_resource = files("geotz.data").joinpath("geonames_all_countries_sorted.tsv")
-    with as_file(data_resource) as path:
-        yield str(path)
+    with as_file(files("geotz.data")) as data_dir:
+        gzip_resource = data_dir.joinpath("geonames_all_countries_sorted.tsv.gz")
+        data_resource = data_dir.joinpath("geonames_all_countries_sorted.tsv")
+
+        if not data_resource.exists():
+            _unzip(gzip_resource, data_resource)
+
+        yield str(data_resource)
+
+
+def _unzip(gzpath: Path, destpath: Path) -> None:
+    with gzip.open(gzpath, "rb") as input, open(destpath, "wb") as output:
+        output.writelines(input)
